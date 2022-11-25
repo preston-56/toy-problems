eb6130305ee97d29f23190302e3c37e7eb276dd1
Preston <prestonosoro56@gmail.com>
2022-11-25 17:22:20 +0300

    my first solution

---
+++ solutions/sol1.js
@@ -0,0 +1,25 @@
+const score = parseInt(prompt('Enter a value between 0 and 100'));
+function studentScore(score) {
+    if(score> 79) {
+        console.log('Grade: A')
+    } 
+    
+    else if(score >=60) {
+        console.log('Grade: B')
+    } 
+    
+    else if(score >=49) {
+        console.log('Grade: C')
+    } 
+    
+    else if(score >=40 ) { 
+
+        console.log('Grade: D')
+    } 
+    
+    else if(score < 40) {
+        console.log('Grade: E')
+    }
+};
+
+studentScore(score);
